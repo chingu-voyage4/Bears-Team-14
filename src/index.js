ef37b0719a0eb0f1af78aa2af2b1e25ef767b028
Shashank Shekhar <shashank7200@gmail.com>
2018-03-27 23:48:51 +0530

    minor refractoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+/**
+ * This File serves as the entry point for the
+ * Speech recognition app.
+ *
+ * © Bears Team 14  - Chingu Voyage 4
+ *
+ * Authors: Atharva Pandey ( github.com/atharvajava ) 
+ *          Shashank Shekhar ( shashank7200.github.io ), 
+ *          Adeen Shukla ( github.com/adeen-s )
+ */
+
+// ==========================={ Imports }============================ //
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+// ======================= { Initial rendering } ===================== //
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
