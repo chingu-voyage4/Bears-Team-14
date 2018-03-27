f9a3b595e0621b770a3ee1562b8343ec956a2f20
Shashank Shekhar <shashank7200@gmail.com>
2018-03-27 23:49:36 +0530

    created the message list component

---
+++ src/components/MessageList.jsx
@@ -0,0 +1,37 @@
+// ===================={ Imports }==================== //
+
+import React, { Component } from 'react';
+import Message from './Message';
+
+// =================== { Component Setup } =========== //
+
+class MessageList extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      time: (new Date()).toDateString()
+    }
+  }
+
+  componentDidUpdate() {
+		ReactDOM.findDOMNode(this).scrollTop = 10000;
+	}
+
+  render(){
+    const { data } = this.props;
+    const { time } = this.state;
+
+    return(
+      <div ref="messageContainer" className="message-container">
+				<div className="time-float">{time}</div>
+				{data.map((item) => (
+					<Message message={item.text} key={item.id} timeStamp={item.id} className={item.class.concat(" user right")} />
+				))}
+			</div>
+    );
+  }
+}
+
+export default MessageList;
