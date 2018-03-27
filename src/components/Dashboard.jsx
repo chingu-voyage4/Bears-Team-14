347ed18e104b77534a2f933e0eb4de272d6c87de
Shashank Shekhar <shashank7200@gmail.com>
2018-03-27 23:49:13 +0530

    created the dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,65 @@
+// ===================={ Imports }==================== //
+
+import React, { Component } from 'react';
+import MessageList from './MessageList';
+
+// =================== { Component Setup } =========== //
+
+class Dashboard extends Component{
+  constructor(props){
+    super(props);
+
+    this.state ={
+      message: '',
+      data: [],
+      class: "blue"
+    }
+
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(event){
+    event.preventDefault();
+
+    const inputMessage = this.refs.inputMessage;
+		this.setState({message: inputMessage.value});
+		
+		if(!inputMessage.value.trim()) {
+			return
+		}
+		
+		var newMessage = {
+			text: inputMessage.value,
+			id: Date.now(),
+			class: this.state.class
+		}
+		
+		this.setState((prevState) =>({
+			data: prevState.data.concat(newMessage),
+			message: ''
+		}));
+		
+		inputMessage.value = '';
+  }
+
+  render(){
+
+    const { data } = this.state;
+
+    return(
+      <div className="speech-recog-dashboard">
+      <div className="chat-app">
+				<MessageList data={ data } />
+				<div className="message-form">
+					<form onSubmit={this._handleSubmit}>
+					  <input ref="inputMessage" type="text" placeholder="Type a message..."/>
+						<button><i className="fa fa-paper-plane"></i></button>
+					</form>
+				</div>
+			</div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
